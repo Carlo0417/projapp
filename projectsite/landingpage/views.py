5e145256655b58f5755fa6e680e4e125bb2e87aa
Kristine Joy Martinez <kjvmartinez@Kristines-MacBook-Air-5.local>
2022-09-26 15:37:53 +0800

    fixed hope so

--- projectsite/landingpage/views.py
+++ projectsite/landingpage/views.py
@@ -1,3 +1,66 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
-# Create your views here.
+from django.views.generic.base import TemplateView
+from django.views.generic.list import ListView
+from django.views.generic.edit import UpdateView
+
+from dormitory.models import Room
+from django import forms
+from dormitory.forms import RoomForm
+from django.contrib import messages
+
+class HomePageView(ListView):
+    model = Room
+    context_object_name = 'room'
+    template_name = "landingpage/home.html"
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        return context
+
+
+class RoomList(ListView):
+    model = Room
+    context_object_name = 'room'
+    template_name = 'room_list.html'
+    paginated_by = 10
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        return context
+
+    def get_queryset(self, *args, **kwargs):
+        qs = super(RoomList, self).get_queryset(*args, **kwargs)
+        qs = qs.order_by("room_name")
+        if self.request.GET.get("q") != None:
+            query = self.request.GET.get('q')
+            qs = qs.order_by("room_name").filter(Q(room_name__icontains=query))
+        return qs
+
+class RoomUpdateView(UpdateView):
+    model = Room
+    fields = "__all__"
+    context_object_name = 'room'
+    template_name = 'room_update.html'
+    success_url = "/room_list"
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        return context
+
+def add_room(request):
+    if request.method == "POST":
+        form = RoomForm(request.POST)
+
+        if form.is_valid():
+            form.save()
+            messages.success(request, 'Form submission successful.')
+            return redirect('RoomAdd')
+
+        else:
+            messages.error(request, 'Please complete required field.')
+            return redirect('RoomAdd')
+    else:
+        form = RoomForm()
+        return render(request, 'room_add.html',  {'form': form})
+
