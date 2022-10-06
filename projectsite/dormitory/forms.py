6fb3bfcc8b072385f364efabc6fd2d67872b7e67
Carlo0417 <201980120@psu.palawan.edu.ph>
2022-10-06 11:01:53 +0800

    admission_req

--- projectsite/dormitory/forms.py
+++ projectsite/dormitory/forms.py
@@ -24,7 +24,15 @@ class OccupantForm(ModelForm):
         model = Occupant
         fields = ['person','bed','start_date','end_date']
 
-class RegistrationForm(ModelForm):
+class RegistrationForm(forms.ModelForm):
+    Field1 = forms.BooleanField()
+    Field2 = forms.BooleanField()
+    Field3 = forms.BooleanField()
+    Field4 = forms.BooleanField()
+    Field5 = forms.BooleanField()
+    Field6 = forms.BooleanField()
+    Field7 = forms.BooleanField()
+
     class Meta:
         model = Person
         fields = "__all__"
